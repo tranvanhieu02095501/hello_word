219dd4007c9dd48dae452decf62c6d91fe8830ef
tranvanhieu02095501 <tranvanhieu02095501@gmail.com>
2021-09-16 18:20:09 +0700

    fix

--- helloworld/src/helloworld/password.java
+++ helloworld/src/helloworld/password.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class password {
      public static void main(String[] args) {
-//    	final  String U = "HuyHoang";
-//    	final  String  P = "2021";
+    	final  String U = "HuyHoang";
+    	final  String  P = "2021";
     	 Scanner sc = new Scanner (System.in);
 		System.out.println(" Nhập UserName : ");
 		String username = sc.nextLine();
 		System.out.println(" Nhập PassWord : ");
 		String password = sc.nextLine();
-		if(  username =="HuyHoang" ||  password == "2021") {
+		if(  username == U &&  password == P) {
 			System.out.println("Nhập đúng (u,p)");
 		}else {
 			System.out.println("Nhập sai Username or PassWord");
