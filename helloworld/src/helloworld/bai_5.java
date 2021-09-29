f22a8ae1343706cf6db49a51097026eaad911c58
tranvanhieu02095501 <tranvanhieu02095501@gmail.com>
2021-09-29 14:19:27 +0700

    bai5

---
+++ helloworld/src/helloworld/bai_5.java
@@ -0,0 +1,25 @@
+package helloworld;
+
+import java.util.Scanner;
+
+public class bai_5 {
+	public class floating_point_numbers {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			 
+			 double a,b ;
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.print("Input floating-point number: ");
+		        a = scanner.nextDouble();	
+		        System.out.print("Input floating-point another number: ");
+		        b = scanner.nextDouble();	
+			
+			if ( a==b ) {System.out.println("They are the same  ");}
+			else {System.out.println("They are different");
+			
+		}
+
+	}
+		}
+}
