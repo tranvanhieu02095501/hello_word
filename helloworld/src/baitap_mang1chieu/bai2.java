5b846d21945c80fa08fd272f7d0fb66b9bcd39f3
tranvanhieu02095501 <tranvanhieu02095501@gmail.com>
2021-10-13 16:27:28 +0700

    n

--- helloworld/src/baitap_mang1chieu/bai2.java
+++ helloworld/src/baitap_mang1chieu/bai2.java
@@ -1,5 +1,37 @@
 package baitap_mang1chieu;
 
-public class bai2 {
+import java.util.Scanner;
 
+public class bai2 {
+	public static void InputArray(int []arr) {
+		
+		Scanner scanner = new Scanner(System.in);
+		for (int i=0; i<=arr.length-1; i++) {
+			System.out.print("a[" + i + "] = ");
+			arr[i] = scanner.nextInt();
+			
+		}
+		scanner.close();
+	}
+	public static void Average(int []arr) {
+		double average = 0;
+		for(int i=0; i<=arr.length-1; i++) {
+			average += arr[i];
+		}
+		System.out.println("Average of array is " + (average / arr.length));
+	}
+	
+	public static void OutputArray(int []arr) {
+		for (int i=0; i<=arr.length-1; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []arr = new int[5];
+		InputArray(arr);
+		OutputArray(arr);
+		System.out.println();
+		Average(arr);
+	}
 }
--- helloworld/src/baitap_mang1chieu/bai3.java
+++ helloworld/src/baitap_mang1chieu/bai3.java
@@ -1,5 +1,56 @@
 package baitap_mang1chieu;
+import java.util.Scanner;
+
 
 public class bai3 {
+	
 
+	
+	public class Bai1_Mang_TimKiem {
+		
+		public static void InputArray(int []arr) {
+			for (int i=0; i<=arr.length-1;i++) {
+				double randomDouble = Math.random();
+	            randomDouble = randomDouble * 100 + 1;
+	            int randomInt = (int) randomDouble;
+				arr[i] = randomInt;
+			}
+			
+		}
+		
+		public static void Find(int []arr, int item) {
+			int index = 0;
+			for (int i=0; i<=arr.length-1; i++) {
+				if (arr[i] == item) {
+					index = i;
+					break;
+				} else {
+					index = -1;
+				}
+			}
+			System.out.println("Chỉ số tìm kiếm được là: " + index);
+		}
+		
+		public static void OutputArray(int []arr) {
+			for (int i=0; i<=arr.length-1; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			
+		}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("�?ộ dài mảng = ");
+			int n = scanner.nextInt();
+			int[] arr = new int[n];
+			InputArray(arr);
+			OutputArray(arr);
+			System.out.println();
+			System.out.print("Số cần tìm = ");
+			int item = scanner.nextInt();
+			Find(arr, item);
+			scanner.close();
+		}
+
+	}
 }
